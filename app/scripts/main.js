3cbcc5682ebafd422c5165f9e38ed3195dca2763
Casper Purtlebaugh <cassie.purtlebaugh@gmail.com>
2017-02-05 17:26:39 -0800

    rm console logs from main.js file

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -11,10 +11,10 @@ jQuery(document).ready(function($) {
   // Switch section
   $('a', '.mainmenu').click(function()
   {
-    console.log('has class: ', $(this).hasClass('active'))
+    // console.log('has class: ', $(this).hasClass('active'))
     if( ! $(this).hasClass('active') ) {
       current_item = this;
-      console.log('this: ', this)
+      // console.log('this: ', this)
       // close all visible divs with the class of .section
       $('.section:visible').fadeOut( section_hide_time, function() {
         $('a', '.mainmenu').removeClass( 'active' );
